30ead073102a3ef3d2bf80daf14502b2b3be2c55
m-mohtashami <m.mohtashami88@gmail.com>
2023-03-07 08:38:38 +0330

    add a test data row to table manualy

--- TODO-App/src/layout/table/index.js
+++ TODO-App/src/layout/table/index.js
@@ -1,4 +1,6 @@
 import { El } from '@/library';
+import { Button } from '@/component';
+import { trash, edit, view } from '@/assets';
 
 export const tableOfContents = () => {
   return El({
@@ -43,6 +45,78 @@ export const tableOfContents = () => {
       El({
         element: 'tbody',
         id: 'tbody',
+        children: [
+          El({
+            element: 'tr',
+            children: [
+              El({
+                element: 'td',
+                className: 'border-2 text-left p-4 pl-8',
+                innerText: 'Task Name',
+              }),
+              El({
+                element: 'td',
+                className: 'border-2 text-center',
+                children: [
+                  El({
+                    element: 'span',
+                    className: 'border bg-gray-200 px-4 py-2 rounded-full',
+                    innerText: 'Low',
+                  }),
+                ],
+              }),
+              El({
+                element: 'td',
+                className: 'border-2 text-center',
+                children: [
+                  El({
+                    element: 'span',
+                    className: 'border bg-blue-600 px-4 py-2 rounded-full',
+                    innerText: 'ToDo',
+                  }),
+                ],
+              }),
+              El({
+                element: 'td',
+                className: 'border-2 text-center',
+                children: [
+                  El({
+                    element: 'span',
+                    className: 'border border-blue-600 px-4 py-2 rounded-full',
+                    innerText: '1400/12/08',
+                  }),
+                ],
+              }),
+              El({
+                element: 'td',
+                className: 'border-2',
+                children: [
+                  El({
+                    element: 'div',
+                    className: 'flex gap-2 items-center justify-center',
+                    children: [
+                      Button({
+                        className:
+                          'bg-red-500 rounded-md p-2 [&_svg]:fill-white my-auto',
+                        innerHTML: trash,
+                      }),
+                      Button({
+                        className:
+                          'bg-blue-500 rounded-md p-2 [&_svg]:fill-white my-auto',
+                        innerHTML: edit,
+                      }),
+                      Button({
+                        className:
+                          'bg-gray-500 rounded-md p-2 [&_svg]:fill-white my-auto',
+                        innerHTML: view,
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          }),
+        ],
       }),
     ],
   });
